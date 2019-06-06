feb3e3329c6ed8d2b24d8898e587d3a90a4db8bf
Kevin <kevinja9608@gmail.com>
2019-06-05 19:04:14 -0600

    Faltan tests

--- lib/stomp_client.dart
+++ lib/stomp_client.dart
@@ -1,29 +1,25 @@
 library stomp_client;
 
+import 'dart:collection';
+
 import 'package:web_socket_channel/io.dart';
 import 'package:flutter/material.dart';
-import 'package:web_socket_channel/web_socket_channel.dart';
 
-/// A Calculator.
+/// A Stomp Client.
 class StompClient {
   IOWebSocketChannel channel;
   Stream<dynamic> stream;
+  HashMap<String, int> _topics;
+  int _topicsCount;
+
   StompClient({@required urlBackend}) {
     channel = IOWebSocketChannel.connect(urlBackend);
     stream = channel.stream;
-    channel.stream.listen((message) {
-      // handling of the incoming messages
-      print(message);
-    }, onError: (error, StackTrace stackTrace) {
-      // error handling
-      print(error);
-    }, onDone: () {
-      // communication has been closed
-      print("Closed");
-    });
+    _topics = HashMap();
+    _topicsCount = 0;
   }
 
-  void connectWithToken(String token) {
+  void connectWithToken({@required String token}) {
     channel.sink.add("CONNECT\n" +
         "Authorization:Bearer " +
         token +
@@ -34,12 +30,45 @@ class StompClient {
         "\x00");
   }
 
-  void subscribe(String topic) {
-    channel.sink.add("SUBSCRIBE" +
-        "id:" + 0.toString() + "\n" +
-        "destination:"  + topic + "\n" +
-        "ack:auto\n" +
-        "\n" +
+  void disconnect() {
+    channel.sink.add("DISCONNECT\n" + 
+        "\n" + 
         "\x00");
+    //channel.sink.close();
+  }
+
+  void subscribe({@required String topic}) {
+    if (!_topics.containsKey(topic)){
+      _topics[topic] = _topicsCount;
+      channel.sink.add("SUBSCRIBE\n" +
+          "id:" + _topicsCount.toString() + "\n" +
+          "destination:" + topic + "\n" +
+          "ack:auto\n" +
+          "\n" +
+          "\x00");
+      _topicsCount++;
+    }
+  }
+
+  void unsubscribe({@required String topic}) {
+    if (_topics.containsKey(topic)){
+      channel.sink.add("UNSUBSCRIBE\n" +
+          "id:" + _topics[topic].toString() + "\n" +
+          "\n" +
+          "\x00");
+      _topics.remove(topic);
+    }
+  }
+
+  void send({@required String topic, String message}) {
+    if (_topics.containsKey(topic)){
+      channel.sink.add("SEND\n" +
+          "destination:" + topic + "\n" +
+          "content-type:text/plain\n" +
+          "\n" +
+          message + "\n" +
+          "\n" +
+          "\x00");
+    }
   }
 }
